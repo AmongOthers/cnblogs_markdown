84c40cf3c71cfe12a5ffb7aa5dbeb8a0a6882490
amongothers <zhengwenwei.123@gmail.com>
2013-05-27 22:32:32 +0800

    -amend

--- showdown.js
+++ showdown.js
@@ -75,7 +75,7 @@ $(function() {
 		$$.replaceWith(converter.makeHtml($$.html().replaceAll("<br>", "\n")
 			.replaceAll("<br />", "\n").replaceAll("&gt;", ">")));
     });
-    $("#postCon p").each(function() {
+    $(".postCon p").each(function() {
 		var $$ = $(this);
 		$$.replaceWith(converter.makeHtml($$.html().replaceAll("<br>", "\n")
 			.replaceAll("<br />", "\n").replaceAll("&gt;", ">")));
