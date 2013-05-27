0433ad0cc348ab694196a237413560d76f17fcb1
amongothers <zhengwenwei.123@gmail.com>
2013-05-27 21:11:01 +0800

    amend

--- showdown.js
+++ showdown.js
@@ -65,20 +65,9 @@ var Showdown={extensions:{}},forEach=Showdown.forEach=function(a,b){if(typeof a.
 //from now on, add our function
 $(function() {
     var converter = new Showdown.converter();
-    $("#cnblogs_post_body").html(converter.makeHtml($.trim($("#cnblogs_post_body").html())));
+    //$("#cnblogs_post_body").html(converter.makeHtml($.trim($("#cnblogs_post_body").html())));
     $("#cnblogs_post_body p").each(function() {
-    var $$ = $(this);
-    $$.html(converter.makeHtml($.trim($$.text())));
+		var $$ = $(this);
+		$$.html(converter.makeHtml($$.html().replace("<br>", "\\n")));
     });
-    $("#cnblogs_post_body pre").each(function() {
-      var language = $(this).attr("class");
-       if(language != null && language.toLowerCase().indexOf("brush") < 0) {
-      $(this).removeClass(language).addClass("brush:" + language + ";gutter:true;");
-    }
-    });
-    if($.browser.msie && $.browser.version < 9.0 && $("#cnblogs_post_body").length > 0) {
-    $("#cnblogs_post_body").prepend($("#myIEWarning").show());
-    } else {
-    $("#myIEWarning").hide();
-    }
 });
